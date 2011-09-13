987606ce1975724233445aec04b2cb8fb8206621
Paul Zhang <ypzhang2@gmail.com>
2011-09-12 18:20:42 -0700

    turn down the request to the lowest level

--- lib/halberd.rb
+++ lib/halberd.rb
@@ -440,14 +440,14 @@ module Halberd
                                                           }],
                                             :attributes! => {:elements => {'xsi:type' => 'dataservice:ContainerCriteria'}}
                                            },
-                     :history_needed => true,
+                     :history_needed => false,
                      :deleted_item_accounts_needed => false,
                      :include_disabled_items => false,
-                     :content_service_info_required => true,
-                     :data_service_lite => true,
+                     :content_service_info_required => false,
+                     :data_service_lite => false,
                      :inactive_item_accounts_needed => false,
-                     :include_is_historic_bill_needed => true,
-                     :include_shared_accounts => true,
+                     :include_is_historic_bill_needed => false,
+                     :include_shared_accounts => false,
                      :attributes! => { :containerCriteria => {"xsi:type" => "collections:List" }}
                     },
             :item_ids => {
