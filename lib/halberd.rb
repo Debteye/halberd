c37d7883cfc0cb67298c90540cd3719fde39453e
Paul Zhang <ypzhang2@gmail.com>
2012-01-24 19:06:02 -0800

    get all content service list

--- lib/halberd.rb
+++ lib/halberd.rb
@@ -171,6 +171,35 @@ module Halberd
         @us = us
       end
 
+      def get_all_content_service_list
+        @all_ervice_list ||= content_traversal_client.request :sl, :get_content_services_by_container_type do
+          soap.element_form_default = :unqualified
+          soap.namespaces['xmlns:tns1'] = "http://collections.soap.yodlee.com"
+          soap.namespaces['xmlns:login'] = 'http://login.ext.soap.yodlee.com'
+          soap.body = {
+            :cctx => {
+              :cobrand_id      => credentials.cobrand_id,
+              :channel_id      => us.channel_id,
+              :locale          => credentials.locale,
+              :tnc_version     => credentials.tnc_version,
+              :application_id  => credentials.application_id,
+              :cobrand_conversation_credentials => {
+                :session_token => us.session_token,
+              },
+              :order! => [:cobrand_id, :channel_id, :locale, :tnc_version, :application_id, :cobrand_conversation_credentials],
+              :attributes! => {
+                :locale => { "xsi:type" => "tns1:Locale" },
+                :cobrand_conversation_credentials => { "xsi:type" => "login:SessionCredentials" }
+              }
+            },
+            :attributes! => {
+              :cobrand_context => { "xsi:type" => "tns1:CobrandContext" }
+            }
+          }
+        end
+      end
+
+
       def get_category_list
         @category_list = category_client.request :sl, :get_supported_transaction_categrories do
           soap.element_form_default = :unqualified
