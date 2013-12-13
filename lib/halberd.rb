2509dba4da3a511c4a440970402f7da2bed0ad7f
John Busch <john.busch@avantcredit.com>
2013-12-13 11:35:56 -0600

    fix method

--- lib/halberd.rb
+++ lib/halberd.rb
@@ -637,6 +637,61 @@ module Halberd
         end
       end
 
+      def update_item_credentials_and_start_verification_data(item_id, opts = {})
+        user_credentials = opts[:credentials]
+                   
+        user_credentials && user_credentials.map! do |credential|
+          CREDENTIAL_ORDER.inject({}) do |hsh, key|
+            hsh[CREDENTIAL_CONVERT[key] || key] = credential[key]
+            hsh
+          end
+        end
+
+        update_response = extended_instant_verification_client.request :sl, :add_item_and_start_verification_data_request do
+          soap.element_form_default = :unqualified
+          soap.namespaces['xmlns:tns1'] = "http://collections.soap.yodlee.com"
+          soap.namespaces['xmlns:login'] = 'http://login.ext.soap.yodlee.com'
+          soap.namespaces['xmlns:common'] = 'http://common.soap.yodlee.com'
+          soap.body = {
+            :user_context => {
+              :cobrand_id      => credentials.cobrand_id,
+              :channel_id      => us.channel_id,
+              :locale          => credentials.locale,
+              :tnc_version     => credentials.tnc_version,
+              :application_id  => credentials.application_id,
+              :cobrand_conversation_credentials => {
+                :session_token => us.session_token,
+              },
+              :preference_info => prefs,
+              :fetch_all_locale_data => false,
+              :conversation_credentials => {
+                :session_token => you.session_token 
+              },
+              :valid => true,
+              :is_password_expired => false,
+              :attributes! => {
+                :locale => { "xsi:type" => "collections:Locale" },
+                :cobrand_conversation_credentials => { "xsi:type" => "login:SessionCredentials" },
+                :conversation_credentials => { "xsi:type" => "login:SessionCredentials" }
+              }
+            },
+            :item_id => item_id,            
+            :credential_fields => {
+              :elements => user_credentials,
+              :attributes! => {
+                :elements => { "xsi:type" => "common:FieldInfoSingle" },
+              }
+            },                    
+            :order! => [:user_context, :credential_fields, :item_id],
+            :attributes! => {
+              :user_context => { "xsi:type" => "common:UserContext" },
+            }
+          }
+        end
+
+        update_response
+      end
+
       def get_instant_account_verification_item!(item_id, opts = {})
         iav_items = [item_id]
 
