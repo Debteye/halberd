54dc866524b471f69f992ab9530c574260032561
John Busch <john.busch@avantcredit.com>
2013-12-13 10:41:47 -0600

    fix method

--- lib/halberd.rb
+++ lib/halberd.rb
@@ -638,8 +638,6 @@ module Halberd
       end
 
       def get_instant_account_verification_item!(item_id, opts = {})
-        iav_items = [item_id]
-
         return_item = instant_verification_client.request :sl, :get_item_verification_data do
           soap.element_form_default = :unqualified
           soap.namespaces['xmlns:tns1'] = "http://collections.soap.yodlee.com"
@@ -668,10 +666,8 @@ module Halberd
                 :conversation_credentials => { "xsi:type" => "login:SessionCredentials" }
               }
             },
-            :item_ids => {
-              :elements => iav_items 
-            },      
-            :order! => [:user_context, :item_ids],
+            :item_id => item_id,
+            :order! => [:user_context, :item_id],
             :attributes! => {
               :user_context => { "xsi:type" => "common:UserContext"}
             }
